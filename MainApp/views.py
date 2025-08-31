949462189697aa3b543fd3ace1c051fd4009cb9e
Vladimir Vasin <ossminoga88@gmail.com>
2025-08-31 13:57:04 +0300

    Done.M1:task3-4

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+author = {
+    "Имя": "Владимир",
+    "Отчество": "Сергеевич",
+    "Фамилия": "Васин",
+    "телефон": "8-916-678-85-17",
+    "email": "ossminoga@rambler.ru",
+}
+
+
+items = [
+    {"id": 1, "name": "Кросовки Adidas", "quantity": 5},
+    {"id": 2, "name": "Куртка кожаная", "quantity": 2},
+    {"id": 5, "name": "Coca-cola 1 литр", "quantity": 12},
+    {"id": 7, "name": "Картофель фри", "quantity": 0},
+    {"id": 8, "name": "Кепка", "quantity": 124},
+]
+
+def home(request):
+    text = """
+    <h1>"Изучаем django</h1>
+    <strong>Автор</strong>: <i>Васин В.С.</i>
+    """
+    return HttpResponse(text)
+
+
+def about(request):
+    text = f"""
+        Имя: {author["Имя"]}<br>
+        Отчество: {author["Отчество"]}<br>
+        Фамилия: {author["Фамилия"]}<br>
+        телефон: {author["телефон"]}<br>
+        email: {author["email"]}<br>
+        """
+    return HttpResponse(text)
+
+
+def get_item(request, item_id: int):
+    """По указанному id возвращает элемент из списка"""
+    for item in items:
+        if item["id"] == item_id:
+            result = f"""
+            <h2> Имя: {item["name"]} </h2>
+            <p> Количество: {item["quantity"]}</p> 
+            """
+            return HttpResponse(result)
+    return HttpResponseNotFound(f'Item with id={item_id} not found')
+
+
